776ff1f14bc3324f2bc952071c9ae62c360be4ba
samiullah <kalhorosami5@gmail.com>
2021-09-14 11:30:54 +0500

    Initial commit

---
+++ app/src/main/java/com/example/cryptocurrencyapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.cryptocurrencyapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.cryptocurrencyapp.ui.theme.CryptocurrencyAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CryptocurrencyAppTheme {
+                Surface(color = MaterialTheme.colors.background) {
+
+                }
+            }
+        }
+    }
+}
